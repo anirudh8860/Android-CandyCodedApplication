d103431eef6bc236f99352c889d6ba5db94a91f9
Anirudh Sohil <anirudh.sohil@gmail.com>
2019-12-03 12:29:45 +0530

    Added dialer intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,7 +26,7 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
+    // DONE - Task 2 - Launch the Google Maps Activity
     // ***
     public void createMapIntent(View view) {
         Uri uri  = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
@@ -40,4 +40,9 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(dialIntent);
+    }
 }
